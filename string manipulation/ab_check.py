ccd7b1d4972388290259e43483812bcf31d0de30
Aditya V B <151858158+adityavb09@users.noreply.github.com>
2023-11-25 11:39:19 +0530

    Update ab_check.py

--- string manipulation/ab_check.py
+++ string manipulation/ab_check.py
@@ -1,1 +1,29 @@
+/****************************************************************
+ *             CODERBYTE AB CHECKS CHALLENGE                    *
+ *                                                              *
+ * Problem Statement                                            *
+ * Have the function ABCheck(str) take the str parameter being  *
+ * passed and return the string true if the characters a and b  *
+ * are separated by exactly 3 places anywhere in the string at  *
+ * least once (ie. "lane borrowed" would result in true because *
+ * there is exactly three characters between a and b).          *
+ * Otherwise return the string false.                           *
+ *                                                              *
+ * Examples                                                     *
+ * Input 1: after badly                                         *
+ * Output 1: false                                              *
+ *                                                              *
+ * Input 2: Laura sobs                                          *
+ * Output 2: true                                               *
+ *                                                              *
+ ***************************************************************/
 
+import re
+def ABCheck(str1):
+    pattern=re.compile(r'[a].{3}[b]')
+    match=pattern.search(str1)
+    
+    return bool(match)
+print(ABCheck("after badly"))
+print(ABCheck("Laura sobs"))    
+print(ABCheck("lane borrowed"))
